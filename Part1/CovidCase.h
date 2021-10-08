cca8337d919a8e7e56c20b165aee23bfcba8e3fa
Dariana <darianadorin@yahoo.com>
2021-10-08 16:58:09 +0100

    almost part 1 NOT done

--- Part1/CovidCase.h
+++ Part1/CovidCase.h
@@ -5,19 +5,62 @@
 using std::ostream;
 
 // TODO: your code  goes here
+#include <string>
+using std::string;
+#include <cmath>
+
+double toRadians(double l)
+{
+  return l*3.14/180;
+}
 
 class CovidCase {
 private:
   double latitude;
   double longitude;
-  String name;
+  string name;
   unsigned int age;
-  int tested;
+  int time;
 
 public:
-  CovidCase( double latitudeIn, double longitudeIn, String nameIn, int ageIn, int testedIn)
+  CovidCase( double latitudeIn, double longitudeIn, string nameIn, int ageIn, int timeIn):
+            latitude(latitudeIn), longitude(longitudeIn),name(nameIn), age(ageIn),time(timeIn){
+            };
 
-  
+  void print(ostream & o) const
+  {
+    o<<"{"<<latitude<<", "<<longitude<<", \""<<name<<"\", "<<age<<", "<<time<<"}";
+  }
+
+  double getLatitude ()
+  {
+    return latitude;
+  }
+  double getLongitude ()
+  {
+    return longitude;
+  }
+
+   double distanceTo(const CovidCase & other) const
+  {
+        double lat1= toRadians(latitude);
+        double lon1= toRadians(longitude);
+        double lat2= toRadians(other.getLatitude());
+        double lon2= toRadians(other.getLongitude());
+
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double a = pow((sin(dlat/2)), 2) + cos(lat1) * cos(lat2) * pow((sin(dlon/2)), 2);
+        double c = 2 * atan2( sqrt(a), sqrt(1-a) );
+        return 3960 * c;
+
+  }
+
+};
+
+ostream & operator<<(ostream & o, const CovidCase & cc){
+  cc.print(o);
+  return o;
 }
 
 
