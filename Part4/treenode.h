8e6386c2fc4a1d4bf9d97e92b99fb5221d3c8af5
Dariana <darianadorin@yahoo.com>
2021-10-24 17:21:40 +0100

    add sol treenode

--- Part4/treenode.h
+++ Part4/treenode.h
@@ -15,6 +15,42 @@ using std::pair;
 
 // TODO your code for the TreeNode class goes here:
 
+template<typename T>
+class TreeNode{
+public:
+    T data;
+    unique_ptr<TreeNode> leftChild;
+    unique_ptr<TreeNode> rightChild;
+    TreeNode * parent;
+
+    TreeNode(T const & d):data(d), parent(nullptr){
+    }
+
+    void setLeftChild(TreeNode* child)
+    {
+        leftChild.reset(child);
+        child->parent = this;
+    }
+
+    void setRightChild(TreeNode* child)
+    {
+        rightChild.reset(child);
+        child->parent = this;
+    }
+    
+    void write(ostream & print) const {
+        if(leftChild) {leftChild->write(print); }
+        print<<" ";
+        print<<data;
+        print<<" ";
+        if(rightChild) {rightChild->write(print);}
+    }
+
+
+
+};
+
+
 // do not edit below this line
 
 #endif
