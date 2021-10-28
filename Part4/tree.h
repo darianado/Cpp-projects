4832da4c0dc35daaaa507530bb28bec0d30f8b76
Dariana <darianadorin@yahoo.com>
2021-10-28 19:50:10 +0100

    add insert in binary search tree

--- Part4/tree.h
+++ Part4/tree.h
@@ -5,7 +5,61 @@
 
 // TODO your code goes here:
 
+template<typename T>
+class BinarySearchTree{
+private:
+    unique_ptr<TreeNode<T>> root;
+public:
+    BinarySearchTree(){}
 
+    void write(ostream & print)
+    {
+        root->write(print);
+    }
+
+    // TreeNode<T>* find(T const & data)
+    // {
+
+    // }
+
+
+    TreeNode<T>* insert(T const & data)
+    {
+        
+      TreeNode<T>* nou = new TreeNode<T>(data);
+      TreeNode<T>* cur = root.get();
+      while(cur){
+          if(data<cur->data)
+          {
+              if(cur->leftChild==nullptr)
+                {
+                    cur->setLeftChild(nou);
+                    nou->parent = cur;
+                    return nou;
+                }
+              cur= cur->leftChild.get();
+          }
+          else if(cur->data < data){
+               if(cur->rightChild==nullptr)
+                {
+                    cur->setRightChild(nou);
+                    nou->parent = cur;
+                    return nou;
+                }
+              cur=cur->rightChild.get();
+          }
+                else{
+                    delete nou;
+                    return cur;
+                }
+      }
+
+        root.reset(nou);
+        return nou;
+
+    }
+    
+};
 
 
 // do not edit below this line
