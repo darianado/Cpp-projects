b6ccbf564a26c5a46ffcaa9bf9e0c79cb1ab8748
dariana <darianadorin@yahoo.com>
2021-11-11 15:15:37 +0000

    better sudoku 1840 still with set

--- Part5/Sudoku.h
+++ Part5/Sudoku.h
@@ -13,7 +13,11 @@ using std::set;
 #include <iostream>
 using std::cout;
 using std::endl;
+<<<<<<< HEAD
+#include<sstream>
+=======
 #include "SudokuSquare.h"
+>>>>>>> 5c9a7e63a5e9289c31f526f153a0a900485036a3
 
 // TODO: Your Sudoku class goes here:
 
@@ -61,6 +65,7 @@ public:
         return board[i][j].size();
     }
 
+
     bool setSquare(int row, int col, int value)
     {
         board[row][col].clear();
@@ -80,25 +85,141 @@ public:
         return true;
         
     }
+
+
+
+   
+    bool solveDuplicHoriz(int row,int col,int howMany)
+    {
+        int found=1;
+        for(int i=0;i<board.size();i++)
+        {
+            if(i!=col && board[row][i]==board[row][col])
+            {
+                //std::cout<<"found"<<*board[row][col].begin()<<*++board[row][col].begin()<<"row"<<row<<"from col"<<col<<"to col"<<i<<"\n";
+                
+                found++;
+                if(found==howMany)
+                {     
+                   for(int j=0;j<board.size();j++)
+                    {
+                        if(board[row][j]!=board[row][col])
+                        {
+                            
+                            //  board[row][j].erase(board[row][col].begin());
+                            //  board[row][j].erase(++board[row][col].begin());
+                            for(auto x:board[row][col])
+                                board[row][j].erase(x);
+                                //std::cout<<"test ajunge aici \n";
+
+                            if(board[row][j].empty()) return false;
+                            
+                            // if(board[row][j].size()==1)
+                            //     if(!updateSetSquare(row,j,*board[row][j].begin())) 
+                            //         return false;
+                            
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool solveDuplicVert(int row,int col,int howMany)
+    {
+        int found=1;
+        for(int i=0;i<board.size();i++)
+        {
+            if(i!=row && board[i][col]==board[row][col])
+            {
+                found++;
+                if(found==howMany)
+                {     
+                for(int j=0;j<board.size();j++)
+                    {
+                        if(board[j][col]!=board[row][col])
+                        {
+                            for(auto x:board[row][col])
+                                board[j][col].erase(x);
+
+                            if(board[j][col].empty()) return false;
+                        }
+                    }
+                }
+            }
+        }
+         return true;
+    }
+
+    bool solveDuplicBox(int row,int col,int howMany)
+    {
+        int found=1;
+        int boxsize = sqrt(board.size());
+        int rowboxnr = row - row % boxsize;
+        int colboxnr = col - col % boxsize ;
+
+        for(int i=rowboxnr; i< rowboxnr+boxsize; i++)
+        {
+            for(int j=colboxnr; j< colboxnr+boxsize; j++)
+            {
+                if(i!=row || j!=col)
+                    if(board[i][j]==board[row][col])
+                    {  
+                        found++;
+                        if(found==howMany)
+                        {
+                            for(int k=rowboxnr; k< rowboxnr+boxsize; k++)
+                            {
+                                for(int l=colboxnr; l< colboxnr+boxsize; l++)
+                                {
+                                    if(board[k][l]!=board[row][col])
+                                    {
+                                        for(auto x:board[row][col])
+                                            board[k][l].erase(x);
+
+                                        if(board[k][l].empty()) return false;
+                                    }
+                                }
+                            }
+                        }
+                    }
+            }
+        }
+        return true;
+    }
+
+        
+    
     bool updateSetSquare(int row, int col, int value)
     {
         for(int i=0;i< board.size() ;i++)
         {
-            if(i!=col && board[row][i].erase(value)) {
-                
+            if(i!=col && board[row][i].erase(value))
+            {
+                // std::cout<<"deleted"<<value
+                // <<"from row"<<row<<"col"<<i<<"because it was on col"<<col<<"\n";
                 if(board[row][i].empty()) return false;
+                //std::cout<<"test ajunge aici \n";
                 if(board[row][i].size()==1) 
                     if(!updateSetSquare(row,i,*board[row][i].begin())) return false;
+                    else;
+                else if(board[row][i].size()<=(board.size()/3))
+                        if(!solveDuplicHoriz(row,i,board[row][i].size()))return false;
             }
         }
 
         for(int i=0;i< board.size() ;i++)
         {
-            if(i!=row && board[i][col].erase(value)){
-                
+            if(i!=row && board[i][col].erase(value))
+            {
                 if(board[i][col].empty()) return false;
                 if(board[i][col].size()==1)
-                        if(!updateSetSquare(i,col,*board[i][col].begin())) return false;
+                    if(!updateSetSquare(i,col,*board[i][col].begin())) return false;
+                    else;
+                else if(board[row][i].size()<=(board.size()/3))
+                        if(!solveDuplicVert(row,i,board[row][i].size()))return false;
             }
         }
 
@@ -110,22 +231,29 @@ public:
         {
             for(int j=colboxnr; j< colboxnr+boxsize; j++)
             {
-                if(i!=row || j!=col){  
+                if(i!=row && j!=col)
+                {  
 
-                    if(board[i][j].erase(value)){
+                    if(board[i][j].erase(value))
+                    {
                         if(board[i][j].empty()) 
                             return false;
                         if(board[i][j].size()==1 )
                             if(!updateSetSquare(i,j,*board[i][j].begin())) return false;
+                            else;
+                        else if(board[row][i].size()<=(board.size()/3))
+                                if(!solveDuplicBox(row,i,board[row][i].size()))return false;
                     }
                 }
             }
         }
 
         return true;
-
-                
+     
     }
+
+
+
 
     virtual bool isSolution() const override{
         for(int i=0;i< board.size();i++)
@@ -163,10 +291,14 @@ public:
 
         for(auto x: board[row][col])
         {
-            Sudoku* copy = new Sudoku(this);
+           Sudoku* copy = new Sudoku(this);
             if(copy->setSquare(row,col,x))
             {
+                // std::cout<<"am ghicit pe row"<<row<<"col"<<col<<"val"<<x<<std::endl;
                 v.emplace_back(copy);
+                // std::ostringstream s;
+                // copy->write(s);
+                // std::cout<<s.str()<<std::endl;
             }
             else delete copy;
 
@@ -175,6 +307,21 @@ public:
 
         return v;
     }
+
+    virtual int heuristicValue() const override
+    {
+        int c=0;
+        for (int i=0;i<board.size();i++) 
+        {
+            for(int j=0;j<board.size();j++)
+            {
+                if(board[i][j].size()>1) c++;
+            }
+        }
+        return c;
+    }
+
+    
 };
 
 
